aaff05c3fb7bdf5c3bf087b9c21ff78f180b9038
ss <setrotec@outlook.com>
2021-06-23 20:54:11 -0300

    Aprendendo: modulo VueResource, lifecycle hooks,  e promises

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,42 @@
-<!--Arquivo App.vue = single file template ou template de unico arquivo
-que tambem é um modulo e declara dentro dele um componente chamado "app "
-que divide-se em 3 tags:
-
-* <template></template> = Aparencia,
-
-* <script></script> = comportamento,
-
-* <style></style> = estilo -->
-
 <template>
-<!--Qnado há mais de um elemento dentro da tag template,
-  é necessario o uso da tag <div></div> ou <contener></contener>-->
-<div>
-<h1>{{ titulo }}</h1>
-<ul>
 
-  <!--Usando a função v-for
-  para iterar <li></li> lsita não ordenada com o array fotos-->
-    <li v-for= "foto of fotos">
+  <div>
+    <h1>{{ titulo }}</h1>
 
-    <!--Interpolação usando 'V-bind' ou ':' = directiva do veu.js
-    para fazer um interpolação em um atributo -->
-      <img :src="foto.url " :alt="foto.titulo" >
-    </li>
-</ul>
-</div>
+    <ul>
+      <!--key: atributo especial é usado principalmente como uma dica 
+        para o algoritmo DOM virtual do Vue para identificar VNodes 
+        ao comparar a nova lista de nós com a lista antiga-->
+
+      <li v-for="foto of fotos" v-bind:key="foto.id">
+        <img :src="foto.url" :alt="foto.titulo">
+      </li>
+    </ul>
+
+  </div>
 </template>
 
 <script>
-export default{
-  data(){
-    return{
-      titulo: 'Galeriapic',
-      fotos: [
-        {
-        url: 'https://www.agrosete.com.br/wp-content/uploads/2020/12/Cachorro-com-a-l%C3%ADngua-de-fora-%C3%A9-sede-1.jpg',
-        titulo: 'cachorro'
-        },
-        {
-        url: 'https://www.agrosete.com.br/wp-content/uploads/2020/12/Cachorro-com-a-l%C3%ADngua-de-fora-%C3%A9-sede-1.jpg',
-        titulo: 'cachorro1'
-        }
-      ]
+export default {
+
+  data() {
+
+    return {
+
+      titulo: 'AppVallum',
+      fotos: []
     }
+  },
+
+  created() {
+
+    this.$http.get('http://localhost:3000/v1/fotos')
+      .then(res => res.json())
+      .then(fotos => this.fotos = fotos, err => console.log(err));
   }
 }
+
 </script>
 
-
 <style>
-
 </style>
